ef88d7dbd139e4beee9a673f6a4bab121bdfbecb
SeanMgcls <magcalassean@gmail.com>
2024-10-22 10:32:38 +0800

    1st commit

---
+++ src/main/java/com/magcalas/car/AccessingdatamysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magcalas.car;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessingdatamysqlApplicationTests {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessingdatamysqlApplicationTests.class, args);
+	}
+
+}
